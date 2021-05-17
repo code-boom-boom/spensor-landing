6b18ccb34c0980e5f33a96c0b3782dcbf3ece00f
Lovecoding <lqingsong7@gmail.com>
2021-05-16 21:56:20 -0700

    info bar

---
+++ src/components/InfoBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full bg-black flex justify-center items-center">
+    <div class="container flex justify-center items-center my-2">
+      <p class="text-white text-center mx-4">🎉
+        To celebrate Spense's launch, we will be waiving all fees for the entire month of April.
+        <a href="#" class="underline">You will be receiving 100% of the earnings.</a> 🎉
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full relative">
+    <info-bar />
+  </div>
+</template>
+
+<script>
+import InfoBar from "../components/InfoBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    InfoBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
